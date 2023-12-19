f6b633818222d4e1940c3b69e944fdf9539ef07d
Rojashree1807 <rojarojashree1807@gmail.com>
2023-12-19 16:44:05 +0530

    Initial commit

---
+++ WebAppAss9/WebAppAss9/Customer.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppAss9
+{
+    public partial class Customer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            Label1.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Visible = true;
+            Label1.Text = "Customer Name : " + TextBox1.Text;
+            Label1.Text += "Mobile No : " + TextBox2.Text;
+            Label1.Text += "Email Id : " + TextBox3.Text;
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebAppAss9/WebAppAss9/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebAppAss9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebAppAss9")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("234c03b9-bf5b-4b2e-8920-d861e3bbc756")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
